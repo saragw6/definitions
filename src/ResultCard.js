2e33b97813c3f5befab85e3b83ebd0957a3428d9
Sara <saragw6@gmail.com>
2018-02-19 17:01:50 -0500

    creds & ids shown on card

--- src/ResultCard.js
+++ src/ResultCard.js
@@ -12,11 +12,13 @@ class ResultCard extends Component {
         <CardTitle
           title={this.props.term}
         />
-        <CardText>{ this.props.def }</CardText>
+        <CardText>{ this.props.def }<p style={{textAlign: 'right', color: '#606060', fontSize: '16px', paddingTop: '10px'}}>{this.props.name}</p><p style={{textAlign: 'right', color: '#606060', fontSize: '12px', lineHeight: '12px'}}>{this.props.id}</p></CardText>
       </Card>
     </div>
     );
   }
+
+//stop inline styling
 
 }
 
--- src/ResultList.js
+++ src/ResultList.js
@@ -5,7 +5,7 @@ import ResultCard from './ResultCard.js'
 function ResultList(props) {
   const entries = props.entries;
   const listItems = entries.map((entry) =>
-    <ResultCard term={entry["term"]} def={entry["definition"]} />
+    <ResultCard term={entry["term"]} def={entry["definition"]} name={entry["name"]} id={entry["identity"]} /> //just pass the whole json object
   );
 
   return (
