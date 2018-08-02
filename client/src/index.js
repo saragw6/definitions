a6deaf6bad3b498bb965239b706e85079ccf89ff
saragw6 <saragw6@gmail.com>
2018-08-03 00:55:39 +0800

    import babel polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@
 // import ReactDOM from 'react-dom';
 // import App from './App';
 // import './index.css';
-// import 'babel-polyfill';
+import 'babel-polyfill';
 
 // ReactDOM.render(
 //   <App />,
