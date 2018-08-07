a68d60bf366917edf70aa8a39285e44b54264820
saragw6 <saragw6@gmail.com>
2018-08-07 22:52:56 +0800

    add entry id display to potential result card

--- client/src/ResultCard.js
+++ client/src/ResultCard.js
@@ -87,6 +87,7 @@ class ResultCard extends Component {
           title={this.lowerCase(this.props.entry["term"])}
         />
         <div style={{textAlign: 'right', marginTop: '-65px', marginBottom: '25px', paddingRight: '5px'}}>
+         {(this.props.entry["action"] === 1) && this.props.entry["entry_id"]}
          <TooltipButton icon='outlined_flag' tooltip="flag for removal"  onClick={this.reportEntry} style={{minWidth:'30px', padding:'0', paddingLeft:'6px', marginRight:'-6px', color:'#BABABA'}}/>
        </div>
         <CardText>{this.paragraphsAndLinks(this.props.entry["definition"])}<br /><br />{this.defWithLinks(this.props.entry["explanation"])}<p style={{textAlign: 'right', color: '#606060', fontSize: '16px', paddingTop: '10px'}}>{this.props.entry["name"]}</p><p style={{textAlign: 'right', color: '#606060', fontSize: '12px', lineHeight: '12px'}}>{this.props.entry["identity"]}</p></CardText>
