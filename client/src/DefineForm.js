404f974629e78046946aaff39985a0d5769734d5
saragw6 <saragw6@gmail.com>
2018-09-25 18:23:16 -0400

    embedded google form components

--- client/src/DefineForm.js
+++ client/src/DefineForm.js
@@ -4,8 +4,9 @@ class DefineForm extends Component {
 
   render() {
     return(
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKF0yyleI5XdPVtl-bEuQUGy2HZPfnUU-e2sDjL31eLuygUA/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
+      <div style={{"display":"flex","width":"100%","justifyContent":"center"}}>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKF0yyleI5XdPVtl-bEuQUGy2HZPfnUU-e2sDjL31eLuygUA/viewform?embedded=true" width="80%" height="1364px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+      </div>
     );
   }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,6 +4,8 @@ import App from './App';
 // import Home from './Home/Home';
 import Callback from './Callback';
 import PotentialDefs from './PotentialDefs';
+import DefineForm from './DefineForm';
+import RequestForm from './RequestForm';
 import ReportedDefs from './ReportedDefs';
 import About from './About';
 import Auth from './Auth';
@@ -40,6 +42,8 @@ export const makeMainRoutes = () => {
         <div>
           <Route path="/*" render={(props) => {searchHashRedirect(); return <App auth={auth} term={getTermFromPath(props.match.params[0])}/>;} }/>
           <Route path="/potentialdefs" render={(props) => <PotentialDefs auth={auth}/>} />
+          <Route path="/defineform" render={(props) => <DefineForm auth={auth}/>} />
+          <Route path="/requestform" render={(props) => <RequestForm auth={auth}/>} />
           <Route path="/reporteddefs" render={(props) => <ReportedDefs auth={auth}/>} />
           <Route path="/about" render={(props) => <About auth={auth}/>} />
           <Route path="/callback" render={(props) => {
