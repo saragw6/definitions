8cfecc7174138623f6d09d36199e8bdc6cd8924d
gracemgately <gracemgately@gmail.com>
2018-10-29 13:20:08 -0400

    [Grace] Refactor: reported

--- routes/reported.js
+++ routes/reported.js
@@ -1,32 +1,28 @@
 const Router = require('express-promise-router');
 const router = new Router();
-
-const db_url = process.env.DB_URL;
-const { Client } = require('pg');
-
-// var bodyParser = require('body-parser');
-// router.use(bodyParser.json());
-// router.use(bodyParser.urlencoded({extended: true}));
+const pool = require('../db')
 
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
-  const client = new Client({ connectionString: db_url, ssl: true });
-  client.connect();
 
   //var queryString = 'SELECT * FROM potential';
-  var queryString = 'SELECT * FROM entry WHERE action = 4';
+  let queryString = 'SELECT * FROM entry WHERE action = $1';
+  let values = [4];
 
+    pool.connect((err, client, release) => {
+        if (err) return console.error('Error acquiring client', err.stack);
 
-  try {
-    const { rows } = await client.query(queryString);
-    res.json(rows);
-    console.log(rows);
-  } catch (err) {
-    console.error(err.stack);
-    res.status(500).send('Error while retrieving reported entries'); //could make more specific
-  }
+        client.query(queryString, values, (err, result) => {
+            release();
+            if (err) {
+                res.status(500).send('Error while retrieving reported entries'); //could make more specific
+                return console.error('Error executing query', err.stack);
+            }
+            console.log(result.rows);
+            res.json(result.rows);
+        })
+    })
 
-  client.end();
 
 });
