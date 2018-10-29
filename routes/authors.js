cd9e48c824f01a8c828221fde24516d978cdd624
gracemgately <gracemgately@gmail.com>
2018-10-29 13:06:04 -0400

    [Grace]Add: db folder for db connections; Refactor: authors into using pg pool

--- routes/authors.js
+++ routes/authors.js
@@ -1,28 +1,27 @@
 const Router = require('express-promise-router');
 const router = new Router();
-
-const db_url = process.env.DB_URL;
-const { Client } = require('pg');
+const pool = require('../db')
 
 module.exports = router;
 
 router.get('/:id', async (req, res, next) => {
 //return entries with author id or name and identity???
 
-  const client = new Client({ connectionString: db_url, ssl: true });
-  client.connect();
-
   //get entries that match the term or synonyms
-  var queryString = 'SELECT name, identity FROM author WHERE author_id = $1';
-  //var authorQueryString = 'SLECT name, identity FROM author WHERE author_id = $1'
+  let queryString = 'SELECT name, identity FROM author WHERE author_id = $1';
+  let values = [req.params.id];
 
-  try {
-    const { rows } = await client.query(queryString, [req.params.id]);
-    console.log(rows);
-    res.json(rows);
-  } catch (err) {
-    console.error(err.stack);
-    res.status(500).send('Error while retrieving entries'); //could make more specific
-  }
-  client.end();
+    pool.connect((err, client, release) => {
+        if (err) return console.error('Error acquiring client', err.stack);
+
+        client.query(queryString, values, (err, result) => {
+            release();
+            if (err) {
+                res.status(500).send('Error while retrieving entries'); //could make more specific
+                return console.error('Error executing query', err.stack);
+            }
+            console.log(result.rows);
+            res.json(result.rows);
+        })
+    })
 });
