abf900633de2429881d86364f53a37f34357496e
gracemgately <gracemgately@gmail.com>
2018-10-30 10:40:28 -0400

    [Grace] Add routesTest folder for running and testing endpoints; todo: multiple folders for more unit tests etc

--- routesTest/reportedTest.js
+++ routesTest/reportedTest.js
@@ -2,21 +2,22 @@ require('mocha');
 const { expect } = require('chai');
 const request = require('request');
 
-// const app = require('../index');
-// const TEST_PORT = 4000;
-// const startApp = () => app.listen(TEST_PORT);
-// const tearDown = () => app.listen().close();
 
-describe('Reported route', () => {
-    // before(startApp);
-    // after(tearDown);
+const reportedTest = () => {
 
-    it('should return all currently reported entries', (done) => {
-        request('http://localhost:4000/reported' , (error, response) => {
-            expect(response.statusCode).to.equal(200);
-            //expect(response.body).to.equal('[]');
-            done();
+    describe('Reported route', () => {
+
+        it('should return currently reported entries with an action code of 4', (done) => {
+            request('http://localhost:4000/reported' , (error, response) => {
+                expect(response.statusCode).to.equal(200);
+                if (response.body.length){
+                    let actionCode = response.body.split("action")[1][2];
+                    expect(actionCode).to.equal('4');
+                }
+                done();
+            });
         });
     });
+}
 
-});
+module.exports = reportedTest;
