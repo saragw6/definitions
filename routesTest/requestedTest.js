fd57745a6628759cce09de11403733178321f552
gracemgately <gracemgately@gmail.com>
2018-10-30 11:45:06 -0400

    [Grace] Add: test for posting requested entry

--- routesTest/requestedTest.js
+++ routesTest/requestedTest.js
@@ -6,25 +6,25 @@ const requestedTest = () => {
 
     describe('Requested route', () => {
 
-        it('GET: should return requested entries', (done) => {
-            request('http://localhost:4000/requested' , (error, response) => {
+        let term = 'test124';
+
+        it('POST: should request an entry', (done) => {
+            request.post({
+                url: `http://localhost:4000/requested/${term}`
+            }, (error, response) => {
                 expect(response.statusCode).to.equal(200);
-                //TODO: figure out response when item requested
+                expect(response.body).to.equal(`added requested term: ${term}`);
                 done();
             });
         });
 
-        //TODO: Post route
-        // it('POST: should request an entry', (done) => {
-        //     let term = 'test';
-        //     request.post({
-        //         url: 'http:localhost/requested',
-        //         form: { term: 'test' }
-        //     });
-        //     expect(response.statusCode).to.equal(200);
-        //     expect(response.message).to.equal(`added requested term: ${term}`)
-        // });
-
+        it('GET: should return requested entries', (done) => {
+            request('http://localhost:4000/requested' , (error, response) => {
+                expect(response.statusCode).to.equal(200);
+                expect(response.body.includes(term)).to.equal(true);
+                done();
+            });
+        });
     });
 
 }
