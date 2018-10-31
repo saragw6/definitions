4345a31b1b7ccba9d5d130a28720b5af16a4ad36
gracemgately <gracemgately@gmail.com>
2018-10-31 09:51:34 -0400

    [Grace] Refactor: resultcard tooltips into toolbox library file

--- client/src/ReactToolboxLibrary.js
+++ client/src/ReactToolboxLibrary.js
@@ -3,7 +3,8 @@ import theme from './assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import Button from 'react-toolbox/lib/button/Button';
 import Tooltip from 'react-toolbox/lib/tooltip';
+import { Card, CardTitle, CardText} from 'react-toolbox/lib/card';
 
-const TooltipButton = Tooltip(Button);
+const TooltipButton = new Tooltip(Button);
 
-export { Autocomplete, theme, ThemeProvider, TooltipButton };
+export { Autocomplete, theme, ThemeProvider, Button, TooltipButton, Card, CardTitle, CardText };
