e588d9d8699ef6d22879c950c52589cce412399b
gracemgately <gracemgately@gmail.com>
2018-10-31 10:00:02 -0400

    change const to let > const might cause problems with reassignment?

--- client/src/Libraries/ReactToolboxLibrary.js
+++ client/src/Libraries/ReactToolboxLibrary.js
@@ -5,6 +5,6 @@ import Button from 'react-toolbox/lib/button/Button';
 import Tooltip from 'react-toolbox/lib/tooltip';
 import { Card, CardTitle, CardText} from 'react-toolbox/lib/card';
 
-const TooltipButton = new Tooltip(Button);
+let TooltipButton = new Tooltip(Button);
 
 export { Autocomplete, theme, ThemeProvider, Button, TooltipButton, Card, CardTitle, CardText };
