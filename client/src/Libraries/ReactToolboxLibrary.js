ea693f22ccc099d8bb32874082ad870065d36b0d
Paulette Zorrilla <pauletteazb@gmail.com>
2019-02-27 23:40:12 -0500

    [Paulette] Create Request Form component
    Remove the console.log line on InputForm Component
    Add 'handleSubmit' functionality to RequestForm
    Add 'snackBar' component to show feedback message to user in a Form

--- client/src/Libraries/ReactToolboxLibrary.js
+++ client/src/Libraries/ReactToolboxLibrary.js
@@ -3,10 +3,11 @@ import Input from '../assets/react-toolbox/lib/input/Input.js';
 import theme from '../assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import Button from 'react-toolbox/lib/button/Button';
+import Snackbar  from 'react-toolbox/lib/snackbar/Snackbar';
 import Tooltip from 'react-toolbox/lib/tooltip';
 import { Card, CardTitle, CardText} from 'react-toolbox/lib/card';
 import FontIcon from 'react-toolbox/lib/font_icon';
 
 let TooltipButton = new Tooltip(Button);
 
-export { Input, Autocomplete, theme, ThemeProvider, Button, TooltipButton, Card, CardTitle, CardText, FontIcon };
+export { Input, Autocomplete, theme, ThemeProvider, Button, Snackbar, TooltipButton, Card, CardTitle, CardText, FontIcon };
--- client/src/Pages/InputFormComponent.js
+++ client/src/Pages/InputFormComponent.js
@@ -3,37 +3,30 @@ import '../stylesheets/Forms.css'; // Import css modules stylesheet as styles
 import { Input } from '../Libraries/ReactToolboxLibrary';
 
 export default class InputForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {label: '', showError: false, errorStyleBck: 'formInput'};
-  }
-
   // Set default props
   static defaultProps = {
-    name: 'name',
     isRequired: false,
     inputType: 'Text',
     labelInput: 'Default question',
     inputHint: 'Your answer',
+    onChange: () => {},
     labelRequiredMessage: 'This is a required question'
   }
 
-  handleChange = (name, isRequired, value) => {
-    console.log('Name', name, ' Val', value, ' isRequired', isRequired)
-    if(value.length === 0 && isRequired)
-      this.setState({...this.state, [name]: value, showError: true, errorStyleBck: 'formInput formInputError'});
-    else
-      this.setState({...this.state, [name]: value, showError: false, errorStyleBck: 'formInput'});
+  handleChange = (label, isRequired, value) => {
+    const {onChange} = this.props;
+    const errorStyle = 'errorStyle'.concat(label);
+    const showError = 'showError'.concat(label);
+    onChange(label, isRequired, showError, errorStyle, value);
   };
 
   render() {
-    const { name, inputType, labelInput, inputHint, labelRequiredMessage, isRequired } = this.props;
-    const { showError, errorStyleBck } = this.state;
+    const { inputType, labelInput, inputHint, labelRequiredMessage, isRequired, value, label, showError, errorStyle } = this.props;
 
     return (
-      <div className={errorStyleBck}>
+      <div className={errorStyle}>
         <label>{labelInput}</label>&nbsp;{(isRequired  && <span className="error">*</span> )}
-        <Input style={{background: 'transparent'}} type={inputType} hint={inputHint} value={this.state.label} name={name} onChange={this.handleChange.bind(this, 'label', isRequired)} maxLength={50} />
+        <Input style={{background: 'transparent'}} type={inputType} hint={inputHint} value={value} onChange={this.handleChange.bind(this, label, isRequired)} maxLength={50} />
         {(showError  &&
           <label className='error errorMessage'>{labelRequiredMessage}</label>)
         }
