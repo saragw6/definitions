eb47419d2ce5a3d18c9e6d5ab98b3d3aa00434d6
JC Holder <jc.holder@thoughtworks.com>
2019-04-18 14:49:33 -0400

    Extract inline CSS from DefineForm

--- client/src/Forms/DefineForm.js
+++ client/src/Forms/DefineForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DefineForm = () => {
     return(
-      <div style={{"display":"flex","width":"100%","justifyContent":"center"}}>
+      <div className="define-form">
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKF0yyleI5XdPVtl-bEuQUGy2HZPfnUU-e2sDjL31eLuygUA/viewform?embedded=true" width="80%" height="1364px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
       </div>
     );
