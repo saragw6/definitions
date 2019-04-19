d4fb4eb3dea71249bf76e7562d64c0f3740b513a
JC Holder <49321798+jcholder-thoughtworks@users.noreply.github.com>
2019-04-19 17:24:13 -0400

    Consolidate all Google form URL values into urls.js (#27)

--- client/src/Forms/DefineForm.js
+++ client/src/Forms/DefineForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { add_definition_url } from "../urls";
+
 const DefineForm = () => {
     return(
       <div className="define-form">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKF0yyleI5XdPVtl-bEuQUGy2HZPfnUU-e2sDjL31eLuygUA/viewform?embedded=true" width="80%" height="1364px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+      <iframe src={{add_definition_url}} width="80%" height="1364px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
       </div>
     );
 }
