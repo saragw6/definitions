1f88ba5db6403d88f703356f085e7175d9bbb6c6
JC Holder <jc.holder@thoughtworks.com>
2019-04-24 16:46:42 -0400

    Prototype definition submission form

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import './stylesheets/queerInput.css';
 import './stylesheets/queerSnackbar.css';
 import './stylesheets/tooltips.css';
 import './stylesheets/resultCard.css';
+import './stylesheets/defineForm.css';
 
 import {Autocomplete, theme, ThemeProvider} from './Libraries/ReactToolboxLibrary';
 import { ResultList } from './Libraries/ComponentsLibrary';
--- client/src/Forms/DefineForm.js
+++ client/src/Forms/DefineForm.js
@@ -1,12 +1,47 @@
 import React from 'react';
 
-import { add_definition_url } from "../urls";
-
 const DefineForm = () => {
     return(
-      <div className="define-form">
-      <iframe src={{add_definition_url}} width="80%" height="1364px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-      </div>
+      <form className="define-form">
+          <p>I'm collecting informal definitions to help people understand LGBTQ+ terms they don't know!</p>
+
+          <p>Help me out by picking a word and telling me what it means to you. Choose any word you think deserves to be in this collection. If you want, you can tell me a bit about your identity to help me make sure I'm including definitions from people with various identities.</p>
+
+          <p>To link to other words use tick marks (NOT single quotes). For example, `LGBTQ+` will link to the definition of LGBTQ+.</p>
+
+          <p>People have requested definitions for these terms:</p>
+
+          <p>Bisexual Jesus | tea | paroromantic | pomosexual  | demifluid | therian | kiki | apressexual | binarism | demiflux | gendervague | pondusgender | omnisensual | pillow princess | bicurious | gynosexual | epicene | ventulian | fingender | wtfsexual | transfeminine | transmasculine | fraysexual | nonbinary femme | body dysphoria | pass | radfem | zedsexual | brotherboy | sistergirl | nblw | split attraction model | juxera | cassgender | metamour | nonbinary girl | greyromantic | intergender | minsexual | alterous attraction | trisexual | lesbian labrys | librafluid | juxera | blue jean femme | ceterosexual | aperiosexual | bellusromantic | genderfuck | two spirit | gendernull | allosexual | queerplatonic | bossy bottom | to read | doe | stag | asexy | generous | biromantic homosexual | fluidflux | aquarigender | arithmagender</p>
+
+          <p className="required">* Required</p>
+
+          <div>
+              <label>Name</label>
+              <span className="subtitle">(Only if you want credit)</span>
+              <input type="text" name="submitter-name" />
+          </div>
+
+          <div className="required">
+              <label>What term are you defining?</label>
+              <input type="text" name="term-name" />
+          </div>
+
+          <div className="required">
+              <label>Define the term and/or say what it means to you:</label>
+              <input type="text" name="term-definition" />
+          </div>
+
+          <div>
+              <label>What has this term meant in your experience? How have you embodied this term?</label>
+              <input type="text" name="term-meaning-to-submitter" />
+          </div>
+
+          <div>
+              <label>How do you identify?</label>
+              <span className="subtitle">(Optional. Ex: race, location, age, gender identity, socioeconomic status, etc)</span>
+              <input type="text" name="submitter-identities" />
+          </div>
+      </form>
     );
 }
 
