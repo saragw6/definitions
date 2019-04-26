f437eaf0ae35c9006102d22f24cc815c037cfa0b
JC Holder <jc.holder@thoughtworks.com>
2019-04-26 15:43:21 -0400

    Revert to Google Forms URL until the form is ready to go live

--- client/src/urls.js
+++ client/src/urls.js
@@ -1,4 +1,4 @@
-const add_definition_url = "/defineform";
+const add_definition_url = "https://docs.google.com/forms/d/e/1FAIpQLSfKF0yyleI5XdPVtl-bEuQUGy2HZPfnUU-e2sDjL31eLuygUA/viewform?usp=sf_link";
 const request_definition_url = "https://goo.gl/forms/xrZyTzaVo8Addq8d2";
 
 export {
