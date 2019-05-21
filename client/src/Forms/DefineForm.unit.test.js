62b7c25c6f421c3c7b2f400620b497e665298591
JC Holder <jc.holder@thoughtworks.com>
2019-05-21 15:25:41 -0400

    Mock InputFormComponent in DefineForm unit test suite to route around multiline bug

--- client/src/Forms/DefineForm.unit.test.js
+++ client/src/Forms/DefineForm.unit.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from "react-test-renderer";
 import DefineForm from './DefineForm';
 
+jest.mock('../Pages/InputFormComponent', () => () => 'InputFormComponent(Mock)');
+
 test('Definition Form?', () => {
   const component = renderer.create(<DefineForm />);
   const jsonForm = component.toJSON();
