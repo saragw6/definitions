1028cb5c18e9aff7ba24bc4530ccbfa056432c00
agarrard <austin.w.garrard@gmail.com>
2019-06-28 14:54:01 -0400

    [Austin/Sara] Update entry status when reporting entry

--- server/routes/reported.js
+++ server/routes/reported.js
@@ -2,6 +2,10 @@ const Router = require('express-promise-router');
 const router = new Router();
 const pool = require('../db')
 
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
@@ -31,8 +35,15 @@ router.post('/', async (req, res) => {
   const now = new Date()
 
   await pool.query(
-    `INSERT INTO report VALUES($1,$2,$3,$4)`,
-    [entryId, email, reason, now])
+    `INSERT INTO report(entry_id, email, reason, time_submited) VALUES($1,$2,$3,$4)`,
+    [entryId, email, reason, now]
+  )
+
+  var entryQueryString = 'UPDATE entry SET action=$1, last_updated=$2 WHERE entry_id=$3'
+  await pool.query(
+    entryQueryString,
+    [4, now, entryId]
+  )
 
   res.json({success: true})
 })
