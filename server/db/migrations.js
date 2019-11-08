b015dd88bbd84d10adac290753f913ac47c4d26f
Adeolu Babatunde <adeolutechs@gmail.com>
2019-11-08 13:48:49 -0600

    Added docker to database init script

--- server/db/migrations.js
+++ server/db/migrations.js
@@ -3,6 +3,7 @@ const addActionToRequested = db => db.query(
   `ALTER TABLE requested
    ADD COLUMN action INTEGER NOT NULL REFERENCES action(id)`
 ).catch(e => {
+  console.log('caught an error e ' + e)
   // ignore the error when we have already run the migration successfully
   if (e.message !== 'column "action" of relation "requested" already exists') {
     throw e
